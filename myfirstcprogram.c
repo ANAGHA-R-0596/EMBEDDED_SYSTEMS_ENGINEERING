03c3444e5a9884f953eb74a994fcce1581e7a4e7
ANAGHA R <anagharajeevan596@gmail.com>
2025-08-05 22:12:03 +0530

    MY FIRST C PROGRAM COMMITED

---
+++ myfirstcprogram.c
@@ -0,0 +1,15 @@
+/*
+AUTHOR : ANAGHA R
+DATE : 05/08/2025
+MY FIRST C PROGRAM
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("NAME => ANAGHA R\n");
+    printf("HOUSE NAME => SIVAM\n");
+    printf("DISTRICT => TRIVANDRUM\n");
+    printf("PINCODE => 695582\n");
+    return 0;
+}
