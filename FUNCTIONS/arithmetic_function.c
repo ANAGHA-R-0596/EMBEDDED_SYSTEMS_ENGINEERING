56d7ab2a715ccf4e24662c0a2780d9c0894c04a5
ANAGHA R <anagharajeevan596@gmail.com>
2025-08-08 21:58:10 +0530

    CALCULATOR

--- FUNCTIONS/arithmetic_function.c
+++ FUNCTIONS/arithmetic_function.c
@@ -13,20 +13,35 @@ int main()
     int ret_sum,ret_diff,ret_prod;
     float ret_quotient;
     int a ,b;
+    int choice;
     printf("ENTER THE FIRST NUMBER=>");
     scanf("%d",&a);
     printf("ENTER THE SECOND NUMBER=>");
     scanf("%d",&b);
-    ret_sum = add(a, b);
-    ret_diff = sub(a, b);
-    ret_prod = mul(a, b);
-    ret_quotient = div(a, b);
-
-    printf("THE RESULT AFTER ADDITION       => %d\n", ret_sum);
-    printf("THE RESULT AFTER SUBTRACTION    => %d\n", ret_diff);
-    printf("THE RESULT AFTER MULTIPLICATION => %d\n", ret_prod);
-    printf("THE RESULT AFTER DIVISION       => %f\n", ret_quotient);
-
+    printf("ENTER THE CHOICE FOR PARTICULAR OPERATION\n");
+    printf(" 1=>ADDITION\n 2=>SUBTRACTION\n 3=>MULTIPLICATION\n 4=>DIVISION\n");
+    scanf(" %d",&choice);
+    if(choice == 1)
+    {
+        ret_sum = add(a, b);
+        printf("THE RESULT AFTER ADDITION => %d\n", ret_sum);
+    }
+    else if(choice == 2)
+    {
+        ret_diff = sub(a, b);
+        printf("THE RESULT AFTER SUBTRACTION => %d\n", ret_diff);
+    }
+    else if(choice == 3)
+    {
+        ret_prod = mul(a, b);
+        printf("THE RESULT AFTER MULTIPLICATION => %d\n", ret_prod);
+    }
+    else if(choice == 4)
+    {
+        ret_quotient = div(a, b);
+        printf("THE RESULT AFTER DIVISION => %f\n", ret_quotient);
+    }
+   
     return 0;
 }
 
