92e44c97e7febe4119cc97fa0dbd5c4a9e6499b9
ANAGHA R <anagharajeevan596@gmail.com>
2025-08-08 12:43:07 +0530

    ADDITION FUNCTION

--- FUNCTIONS/add_function.c
+++ FUNCTIONS/add_function.c
@@ -1,9 +1,18 @@
+/*
+AUTHOR : ANAGHA R
+DATE : 08/08/2025
+FUNCTION TO PERFORM ADDITION OPERATION
+*/
 #include<stdio.h>
 int add(int,int);
 int main()
 {
     int result;
-    int a = 100,b = 65;
+    int a ,b;
+    printf("ENTER THE FIRST NUMBER=>");
+    scanf("%d",&a);
+    printf("ENTER THE SECOND NUMBER=>");
+    scanf("%d",&b);
     result = add(a,b);
     printf("THE RESULT AFTER ADDITION => %d",result);
 }
