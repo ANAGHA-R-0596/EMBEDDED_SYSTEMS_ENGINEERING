e6c71507dbe94f482c5a762adf30e38d97c32a7d
ANAGHA R <anagharajeevan596@gmail.com>
2025-10-09 21:37:21 +0530

    External Interrupt 0 (Falling Edge) LED Toggle

---
+++ AVR_PROGRAMMING/EXT_INTERRUPT_FALLING/EXT_INTERRUPT_FALLING/main.c
@@ -0,0 +1,41 @@
+/*
+ * EXT_INTERRUPT_FALLING.c
+ *
+ * Created: 09-10-2025 16:40:17
+ * Author : anagh
+ */ 
+ 
+
+#define F_CPU 16000000UL
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+
+ISR(INT0_vect) //INTERRUPT SERVICE ROUTINE
+{
+		PORTB ^= (1 << PORTB5); //TOGGLE LED WHEN SWITCH IS PRESSED
+		_delay_ms(10);
+}
+int main(void)
+{
+    DDRB |= (1 << DDB5);  //PB5 AND PB4 IS SET AS OUTPUT
+	DDRD &= ~(1 << DDD2); //PD2 IS SET AS INPUT
+	PORTD |= (1 << PORTD2); // Enable internal pull-up on PD2
+	
+	EIMSK |= (1 << INT0); //ENABLING INTERRUPT 0.
+	// FALLING EDGE TRIGGER FOR INT0
+	EICRA |= (1 << ISC01);
+	sei(); //ENABLING GLOBAL INTERRUPT
+	
+	
+    while (1) 
+    {
+		
+    }
+}
+
+
+
+
